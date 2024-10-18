435491c8e3e49f271af6d4fa0915345888d1e60c
Raulpassos08 <raulfera2@gmail.com>
2024-10-18 13:14:04 -0300

    variant do botão

--- src/app/componentes/newsletter-form/newsletter-form.component.ts
+++ src/app/componentes/newsletter-form/newsletter-form.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-newsletter-form',
   standalone: true,
-  imports: [BtnPrimaryComponent],
+  imports: [BtnPrimaryComponent,
+  ReactiveFormsModule],
   templateUrl: './newsletter-form.component.html',
   styleUrl: './newsletter-form.component.scss'
 })
 export class NewsletterFormComponent {
-
+  
 }
