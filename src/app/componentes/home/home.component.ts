21936414a06bc5103c75492b6c4cca635a337f15
Raulpassos08 <raulfera2@gmail.com>
2024-10-23 12:27:56 -0300

    projeto final cmpleto

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -3,6 +3,9 @@ import { HeaderComponent } from '../header/header.component';
 import { NgOptimizedImage } from '@angular/common';
 import { BtnPrimaryComponent } from "../btn-primary/btn-primary.component";
 import { NewsletterFormComponent } from "../newsletter-form/newsletter-form.component";
+import { HttpClient, HttpClientModule, HttpHandler } from '@angular/common/http';
+import { BenefitComponent } from "../benefit/benefit.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-home',
@@ -11,8 +14,12 @@ import { NewsletterFormComponent } from "../newsletter-form/newsletter-form.comp
     HeaderComponent,
     NgOptimizedImage,
     BtnPrimaryComponent,
-    NewsletterFormComponent
+    NewsletterFormComponent,
+    HttpClientModule,
+    BenefitComponent,
+    FooterComponent
 ],
+providers:  [HttpClient],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
